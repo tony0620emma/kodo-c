5e45527e8d91271bfa7bd4f1b4ef25d5e85adc2e
Peter Vingelmann <peter.vingelmann@gmail.com>
2015-01-27 19:20:14 +0100

    Update configure_and_build_all.sh

--- examples/including_kodo/ios/configure_and_build_all.sh
+++ examples/including_kodo/ios/configure_and_build_all.sh
@@ -5,7 +5,9 @@ DIRECTORY=$PWD
 cd ../../../
 
 for ARCH in i386 x86_64 armv7 armv7s arm64; do
-    python waf configure -p --options=cxx_mkspec=cxx_ios70_apple_llvm60_$ARCH --prefix=/tmp/$ARCH build install --options=install_path=/tmp/$ARCH/,install_static_libs
+    python waf configure --options=cxx_mkspec=cxx_ios70_apple_llvm60_$ARCH
+    python waf build
+    python waf install --options=install_path=/tmp/$ARCH/,install_static_libs
 done
 
 cd $DIRECTORY
