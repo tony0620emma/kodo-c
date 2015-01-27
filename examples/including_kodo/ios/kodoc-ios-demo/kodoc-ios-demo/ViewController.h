9dfc044cda79f764b09401d7a3550f0d69c26e35
Jeppe Krigslund <krigslund@steinwurf.com>
2015-01-27 11:22:53 +0100

    added demo project skeleton

---
+++ examples/including_kodo/ios/kodoc-ios-demo/kodoc-ios-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kodoc-ios-demo
+//
+//  Created by Steinwurf on 22/01/15.
+//  Copyright (c) 2015 Steinwurf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
