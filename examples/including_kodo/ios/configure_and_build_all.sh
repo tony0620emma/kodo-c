b95bb802433f1f33ccb60f0e46f4bb9774b4e487
Jeppe Krigslund <krigslund@steinwurf.com>
2015-01-27 17:11:04 +0100

    removed specifics config from compile and build all script

--- examples/including_kodo/ios/configure_and_build_all.sh
+++ examples/including_kodo/ios/configure_and_build_all.sh
@@ -5,7 +5,7 @@ DIRECTORY=$PWD
 cd ../../../
 
 for ARCH in i386 x86_64 armv7 armv7s arm64; do
-    python waf configure --bundle=ALL,-waf-tools --waf-tools-path ../waf-tools -p --options=cxx_mkspec=cxx_ios70_apple_llvm60_$ARCH --prefix=/tmp/$ARCH build install --options=install_path=/tmp/$ARCH/,install_static_libs
+    python waf configure -p --options=cxx_mkspec=cxx_ios70_apple_llvm60_$ARCH --prefix=/tmp/$ARCH build install --options=install_path=/tmp/$ARCH/,install_static_libs
 done
 
 cd $DIRECTORY
