b141ec75540420e3dcfc5e40b933d6ffff5af300
Peter Vingelmann <petya@master.com>
2015-01-31 03:18:24 +0100

    Add read/write symbol test with predefined coding vectors

--- test/src/test_read_write_uncoded_symbol.cpp
+++ test/src/test_read_write_uncoded_symbol.cpp
@@ -11,8 +11,8 @@
 
 #include "test_helper.hpp"
 
-void test_shallow_api(uint32_t symbols, uint32_t symbol_size,
-                      int32_t code_type, int32_t finite_field)
+void test_read_write_uncoded(uint32_t symbols, uint32_t symbol_size,
+                             int32_t code_type, int32_t finite_field)
 {
     kodo_factory_t encoder_factory =
         kodo_new_shallow_encoder_factory(code_type, finite_field,
@@ -51,32 +51,62 @@ void test_shallow_api(uint32_t symbols, uint32_t symbol_size,
 
     uint32_t payload_size = kodo_payload_size(encoder);
     uint8_t* payload = (uint8_t*) malloc(payload_size);
+    uint8_t* test_symbol = (uint8_t*) malloc(symbol_size);
 
     // Allocate symbols in non-contiguous buffers
     uint8_t** input_symbols = (uint8_t**) malloc(symbols * sizeof(uint8_t*));
     uint8_t** output_symbols = (uint8_t**) malloc(symbols * sizeof(uint8_t*));
 
-    for (uint32_t i = 0; i < symbols; ++i)
+    uint32_t i, j;
+    // Create the original symbols and store them in the encoder
+    for (i = 0; i < symbols; ++i)
     {
         // Create the individual symbols for the encoder
         input_symbols[i] = (uint8_t*) malloc(symbol_size);
 
         // Randomize input data
-        for (uint32_t j = 0; j < symbol_size; ++j)
+        for (j = 0; j < symbol_size; ++j)
             input_symbols[i][j] = rand() % 256;
 
         // Store the symbol pointer in the encoder
         kodo_set_symbol(encoder, i, input_symbols[i], symbol_size);
+    }
 
+    // Transfer the original symbols to the decoder with some losses
+    uint32_t lost_symbols = 0;
+    for (i = 0; i < symbols; ++i)
+    {
         // Create the output symbol buffers for the decoder
         output_symbols[i] = (uint8_t*) malloc(symbol_size);
 
         // Specify the output buffers used for decoding
         kodo_set_symbol(decoder, i, output_symbols[i], symbol_size);
+
+        // Simulate a channel with a 50% loss rate
+        if (rand() % 2)
+        {
+            lost_symbols++;
+            continue;
+        }
+
+        // We test that kodo_write_uncoded_symbol writes the original symbol
+        // to the test buffer
+        kodo_write_uncoded_symbol(encoder, test_symbol, i);
+        // Compare input_symbols[i] with test_symbol
+        EXPECT_EQ(memcmp(input_symbols[i], test_symbol, symbol_size), 0);
+
+        // If the symbol was not lost, then insert that symbol into the decoder
+        // using the raw symbol data (no additional headers are needed)
+        // This will copy the data from input_symbols[i] to output_symbols[i]
+        kodo_read_uncoded_symbol(decoder, input_symbols[i], i);
+
+        EXPECT_EQ(i + 1 - lost_symbols, kodo_rank(decoder));
     }
 
     ASSERT_TRUE(kodo_is_complete(decoder) == 0);
 
+    // Now we generate coded packets with the encoder in order to recover the
+    // lost packets on the decoder side
     while (!kodo_is_complete(decoder))
     {
         kodo_write_payload(encoder, payload);
@@ -86,7 +116,7 @@ void test_shallow_api(uint32_t symbols, uint32_t symbol_size,
     EXPECT_TRUE(kodo_is_complete(decoder) != 0);
 
     // Compare the input and output symbols one-by-one
-    for (uint32_t i = 0; i < symbols; ++i)
+    for (i = 0; i < symbols; ++i)
     {
         EXPECT_EQ(memcmp(input_symbols[i], output_symbols[i], symbol_size), 0);
 
@@ -97,6 +127,7 @@ void test_shallow_api(uint32_t symbols, uint32_t symbol_size,
     free(input_symbols);
     free(output_symbols);
     free(payload);
+    free(test_symbol);
 
     kodo_delete_encoder(encoder);
     kodo_delete_decoder(decoder);
@@ -105,17 +136,17 @@ void test_shallow_api(uint32_t symbols, uint32_t symbol_size,
     kodo_delete_decoder_factory(decoder_factory);
 }
 
-TEST(TestShallowFullRlncCodes, invoke_api)
+TEST(TestReadWriteUncodedSymbol, uncoded_symbols)
 {
     uint32_t symbols = rand_symbols();
     uint32_t symbol_size = rand_symbol_size();
 
-    test_shallow_api(symbols, symbol_size,
-                     kodo_full_rlnc, kodo_binary);
+    test_read_write_uncoded(symbols, symbol_size,
+                            kodo_full_rlnc, kodo_binary);
 
-    test_shallow_api(symbols, symbol_size,
-                     kodo_full_rlnc, kodo_binary8);
+    test_read_write_uncoded(symbols, symbol_size,
+                            kodo_full_rlnc, kodo_binary8);
 
-    test_shallow_api(symbols, symbol_size,
-                     kodo_full_rlnc, kodo_binary16);
+    test_read_write_uncoded(symbols, symbol_size,
+                            kodo_full_rlnc, kodo_binary16);
 }
