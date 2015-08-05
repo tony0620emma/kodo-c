492d0b5b24af9b10b904bd39318ddf1cf1cbd1d9
Jeppe Pihl <jpihl08@gmail.com>
2015-08-05 14:22:23 +0200

    fix copyright year

--- src/kodoc/perpetual_encoder_factory_wrapper.hpp
+++ src/kodoc/perpetual_encoder_factory_wrapper.hpp
@@ -1,4 +1,4 @@
-// Copyright Steinwurf ApS 2014.
+// Copyright Steinwurf ApS 2015.
 // Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
